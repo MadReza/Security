93d578db6f82014397e5821ad99e1c1805168db9
Reza Madabadi <rezmaza@gmail.com>
2016-12-01 01:13:34 -0500

    Fixed Overflow and Added Generate Prime numbers

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,29 @@
 import random
 import math
 
+def mulmod(a, b, c):
+    """calculates (a*b)%c. Solved Math Overflow errors"""
+    x = 0
+    y = a % c
+    
+    while b > 0:
+        if b % 2 == 1:
+            x = (x+y) % c
+        y = (y*2) % c
+        b = b / 2
+    return x % c
+
+def modulo(a, b, c):
+    """Calculates a^b % c. Solved for Math overflow errors"""
+    res = 1
+    i = 0
+
+    while i < b:
+        res = res * a
+        res = res % c
+        i = i + 1
+    return res % c
+
 def Miller(value, iteration):
     """Determins if the value is prime probably using the Rabin-Miller test"""
     """https://www.topcoder.com/community/data-science/data-science-tutorials/primality-testing-non-deterministic-algorithms/"""
@@ -18,10 +41,10 @@ def Miller(value, iteration):
     while iteration:
         a = random.randint(1, value-1)
         temp = s
-        mod = math.pow(a, temp) % value
+        mod = modulo(a, temp, value)
 
         while temp != value-1 and mod != 1 and mod != value-1:
-            mod = (mod*mod) % value
+            mod = mulmod(mod, mod, value)
             temp *= 2
 
         if mod != value-1 and temp%2 == 0:
@@ -29,3 +52,14 @@ def Miller(value, iteration):
 
         iteration = iteration -1
     return True
+
+def generate_prime(k):
+    """generate a number of k bits"""
+    x = k
+    x = x + random.randint(1, 1000)
+
+    while Miller(x, 5) == False:
+        #x = x + 1
+        x = x + random.randint(1, 1000)
+    return x
+
